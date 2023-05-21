a866ad433ff4dc8cba0aff4b9aae97f5aadde354
Milos Pavlovic <milospavlovicdev@gmail.com>
2023-05-21 15:54:19 +0200

    Initial Commit

---
+++ RecordingApp/RecordingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecordingAppApp.swift
+//  RecordingApp
+//
+//  Created by Milos Pavlovic on 21.5.23..
+//
+
+import SwiftUI
+
+@main
+struct RecordingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
